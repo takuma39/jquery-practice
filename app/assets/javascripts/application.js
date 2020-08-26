7d3b3c16873cdb08f1feebf49e1301b4ac0eb8e1
takuma39 <takuma.sato.job@gmail.com>
2020-08-26 06:30:30 +0000

    Add this機能

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -22,21 +22,22 @@
 // });
 
 $(document).on('turbolinks:load', function() {
+	// slideUp / SlideDown
 	$('#btn1').click(function(){
 		if ($('#test1').hasClass('active')){
 			$('#test1').removeClass('active');
 			$('#test1').slideDown(1000);
-			$('#btn1').text('SlideUp');
-			$('#test1').text('slideUp');
+			$('#btn1').text('slideUp');
+			$('#test1 p').text('slideUp');
 		} else{
 			$('#test1').addClass('active');
 			$('#test1').slideUp(1000);
 			$('#btn1').text('slideDown');
-			$('#test1').text('slideDown');
+			$('#test1 p').text('slideDown');
 		}
 	});
 
-
+	// color変更
 	$('#btn2').click(function(){
 		if ($('#test2').hasClass('active')){
 			$('#test2').removeClass('active');
@@ -46,6 +47,40 @@ $(document).on('turbolinks:load', function() {
 			$('#test2').css('color', 'red');
 		}
 	});
+
+	// text変更
+	$('#btn3').click(function(){
+		if ($('#test3').hasClass('active')){
+			$('#test3').removeClass('active');
+			$('#test3 p').text('text');
+		} else{
+			$('#test3').addClass('active');
+			$('#test3 p').text('change text');
+		}
+	});
+
+	// html変更
+	$('#btn4').click(function(){
+		if ($('#test4').hasClass('active')){
+			$('#test4').removeClass('active');
+			$('#test4').html('<p>html<p/>');
+		} else{
+			$('#test4').addClass('active');
+			$('#test4').html('<h3>&lt;h3&gt;html&lt;/h3&gt;</h3>');
+		}
+	});
+
+	// this
+	$('.test5').click(function(){
+		if ($(this).hasClass('active')){
+			$(this).removeClass('active');
+			$(this).css('color', 'black');
+		} else{
+			$(this).addClass('active');
+			$(this).css('color', 'red');
+		}
+	});
+
 });
 
 
